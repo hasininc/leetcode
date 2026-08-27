e39afac7310065a94a90676d8f659469e5a70998
Hasini Nallan <nchasini9j@gmail.com>
2026-08-27 23:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.07%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -9,17 +9,17 @@ public:
         int a = 0;
         int b = 1;
         int c = 1;
-        int sum = 0;
+        
 
         for(int i=3; i<=n; i++){
-            sum = a+b+c;
+            int sum = a+b+c;
 
             a=b;
             b=c;
             c=sum;
         }
 
-        return sum;
+        return c;
         
     }
 };
